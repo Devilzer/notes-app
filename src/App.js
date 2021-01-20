4740629c3b71ac1d9c40c84269f2d0ac490e8b58
Devilzer <jenadeepak724@gmail.com>
2021-01-20 21:27:33 +0530

    add notes list outline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './style/App.scss';
 import NoteInput from "./components/NoteInput";
 import SearchBox from "./components/SearchBox";
-
+import NoteList from "./components/NoteList";
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
       </h1>
       <NoteInput/>
       <SearchBox/>
+      <NoteList/>
     </div>
   );
 }
---
+++ src/components/NoteList.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+function NoteList() {
+    return (
+        <div className="list-container">
+            <div className="note-container">
+                <div className="title">
+                    <h2>
+                        This is title.
+                    </h2>
+                </div>
+                <div className="date-btn">
+                    <h4>
+                        jan 21 2021
+                    </h4>
+                    <div className="btns">
+                        <button><i className="fas fa-pencil-alt edit"></i></button>
+                        <button><i className="far fa-trash-alt delete"></i></button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="note-container">
+                <div className="title">
+                    <h2>
+                        This is title.
+                    </h2>
+                </div>
+                <div className="date-btn">
+                    <h4>
+                        jan 21 2021
+                    </h4>
+                    <div className="btns">
+                        <button><i className="fas fa-pencil-alt edit"></i></button>
+                        <button><i className="far fa-trash-alt delete"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div className="note-container">
+                <div className="title">
+                    <h2>
+                        This is title.
+                    </h2>
+                </div>
+                <div className="date-btn">
+                    <h4>
+                        jan 21 2021
+                    </h4>
+                    <div className="btns">
+                        <button><i className="fas fa-pencil-alt edit"></i></button>
+                        <button><i className="far fa-trash-alt delete"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div className="note-container">
+                <div className="title">
+                    <h2>
+                        This is title.
+                    </h2>
+                </div>
+                <div className="date-btn">
+                    <h4>
+                        jan 21 2021
+                    </h4>
+                    <div className="btns">
+                        <button><i className="fas fa-pencil-alt edit"></i></button>
+                        <button><i className="far fa-trash-alt delete"></i></button>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default NoteList;
