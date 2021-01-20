70dc7a501e24814c9bd6dd64c186f8c33a856159
Devilzer <jenadeepak724@gmail.com>
2021-01-20 17:44:09 +0530

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './style/App.scss';
 import NoteInput from "./components/NoteInput";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         Notes
       </h1>
       <NoteInput/>
+      <SearchBox/>
     </div>
   );
 }
---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBox() {
+    return (
+        <div className="search-container">
+            <input type="text" placeholder="Search title..."/>
+        </div>
+    )
+}
+
+export default SearchBox;
